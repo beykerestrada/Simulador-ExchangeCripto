0463aa6fd899ec4f10e7a710bec78a5daa2683d4
Beyker Estrada <beyker.chirino.estrada@gmail.com>
2022-09-06 03:34:32 -0300

    Se crean arrays para almacenar las cripto en oferta

--- js/index.js
+++ js/index.js
@@ -5,17 +5,29 @@ function simulador() {
     let compra = 0;
     let moneda = 0;
 
+// ARRAY QUE ALMACENA LAS DIFERENTES CRIPTOMONEDAS EN OFERTA COMO OBJETOS
 
+    const currencies = [
+        {nombre: "USDCoin", signo: "USDC", valor: 1},
+        {nombre: "Bitcoin", signo: "BTC", valor: 20067.80},
+        {nombre: "Ether", signo: "ETH", valor: 1487.74},
+    ]
+
+// SE CREA LA CONSTANTE PRECIOS PARA EXTRAER EL PRECIO DE LAS CRIPTOS DESDE EL ARRAY Y PODER USARLO PARA REALIZAR LA CONVERSION MÁS ADELANTE
+    const precios = currencies.map(i => i.valor);
+    console.log(precios);
 
     //Este bucle activa la acción de abonar a la billetera y guarda el valor en la variable saldoUsd
     function abonar() {
+        nombreUsuario = prompt("Ingresa tu nombre");
+        alert(`Bievenid@ ${nombreUsuario}`);
         while (saldoUsd <= 0) {
             saldoUsd = parseInt(prompt("Ingrese el monto a abonar en USD"));
             if (saldoUsd < 10) {
                 alert(`El monto mínimo que puedes abonar son 10 USD`);
                 saldoUsd = 0;
             } else if (Number.isNaN(saldoUsd)) {
-                alert("Por favor ingresa un valor numérico")
+                alert("Por favor ingresa un valor numérico");
                 saldoUsd = 0;
             }
             else {
@@ -56,16 +68,19 @@ function simulador() {
     BTC: 20067.80 USD
     ETH: 1487.74 USD
     USDC: 1 USD
+
+    Este dato se extrae desde la constante "PRECIOS", primero fue filtrado desde el array "CURRENCIES"
+
     */
 
     function convertir(cantidad, moneda) {
 
         switch (moneda) {
             case 1:
-                return cantidad / 20067.80 + " BTC"
+                return cantidad / precios[1] + " BTC";
 
             case 2:
-                return cantidad / 1487.74 + " ETH";
+                return cantidad / precios[2] + " ETH";
 
             case 3:
                 return cantidad + " USDC";
@@ -90,6 +105,8 @@ function simulador() {
     }
     resumen();
 
+    
+    
 
 
 }
